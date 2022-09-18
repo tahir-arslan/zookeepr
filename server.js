f40ca718eb7d535e5788214efb55c4594d001857
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-18 11:38:34 -0400

    completed 11.2

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals');
 const express = require('express');
+// heroku sets variable port to 80 by default
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -40,6 +50,45 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+// can use `.filterByQuery()` but since this route will only return a single animal due to it's
+// it's unique id, no need for the extra code
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    // if we were writing to a larger dataset, async version would be better (`fs.writeFile()`)
+    // `fs.writeFileSync()` is the synchronous version of `fs.writeFile()`
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        // save javascript array data as JSON -> `JSON.stringify()`
+        // null means we don't want to edit any of our existing data, `2` indicates we to create
+        // whtie space between our values to make it more readable
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    // return finished code to POST 
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -48,6 +97,34 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+// if search parameter not found, send 404
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what next index of array will be
+    req.body.id = animals.length.toString();
+
+    // validate data before updating file. if no, send 400. this is important so no invalid data
+    // is accepted and stored, and it also allows for more consistent data to be saved which 
+    // will help to avoid `what if` scenarios that would need to be factored in and coded for
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
+
+// heroku runs app in an environment variable called process.env.PORT. we will tell app
+// to use that port, and if not then use default port of 3001
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
